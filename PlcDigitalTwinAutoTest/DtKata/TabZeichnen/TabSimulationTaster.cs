9d5a03016cee6f3b8c1f3309ee01b184557c57c3
Kurt Linder <kurt.linder@lbsfe1.snv.at>
2021-12-21 18:07:36 +0100

    Ordentlicher Bildschirm!

--- PlcDigitalTwinAutoTest/DtKata/TabZeichnen/TabSimulationTaster.cs
+++ PlcDigitalTwinAutoTest/DtKata/TabZeichnen/TabSimulationTaster.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 
 namespace DtKata.TabZeichnen;
 
@@ -7,21 +8,9 @@ public partial class TabZeichnen
 {
     private static void TabSimulationTaster(Grid grid, BasePlcDtAt.BaseViewModel.ViewModel viewmodel)
     {
-        LibWpf.LibButton.ButtonViz("S1", 2, 5, 2, 3, 20, new Thickness(2, 2, 2, 2),
-            viewmodel.BtnTaster, ViewModel.ViewModel.WpfObjects.S1,
-            $"ClkMode[{(int)ViewModel.ViewModel.WpfObjects.S1}]",
-            System.Windows.Controls.Primitives.ButtonBase.ClickModeProperty, grid);
-        LibWpf.LibButton.ButtonViz("S2", 2, 5, 6, 3, 20, new Thickness(2, 2, 2, 2),
-            viewmodel.BtnTaster, ViewModel.ViewModel.WpfObjects.S2,
-            $"ClkMode[{(int)ViewModel.ViewModel.WpfObjects.S2}]",
-            System.Windows.Controls.Primitives.ButtonBase.ClickModeProperty, grid);
-        LibWpf.LibButton.ButtonViz("S3", 2, 5, 10, 3, 20, new Thickness(2, 2, 2, 2),
-            viewmodel.BtnTaster, ViewModel.ViewModel.WpfObjects.S3,
-            $"ClkMode[{(int)ViewModel.ViewModel.WpfObjects.S3}]",
-            System.Windows.Controls.Primitives.ButtonBase.ClickModeProperty, grid);
-        LibWpf.LibButton.ButtonViz("S4", 2, 5, 14, 3, 20, new Thickness(2, 2, 2, 2),
-            viewmodel.BtnTaster, ViewModel.ViewModel.WpfObjects.S4,
-            $"ClkMode[{(int)ViewModel.ViewModel.WpfObjects.S4}]",
-            System.Windows.Controls.Primitives.ButtonBase.ClickModeProperty, grid);
+        LibWpf.LibButton.ButtonViz("S1", 2, 5, 2, 3, 20, new Thickness(2, 2, 2, 2), viewmodel.BtnTaster, ViewModel.ViewModel.WpfObjects.S1, $"ClkMode[{(int)ViewModel.ViewModel.WpfObjects.S1}]", ButtonBase.ClickModeProperty, grid);
+        LibWpf.LibButton.ButtonViz("S2", 2, 5, 6, 3, 20, new Thickness(2, 2, 2, 2), viewmodel.BtnTaster, ViewModel.ViewModel.WpfObjects.S2, $"ClkMode[{(int)ViewModel.ViewModel.WpfObjects.S2}]", ButtonBase.ClickModeProperty, grid);
+        LibWpf.LibButton.ButtonViz("S3", 2, 5, 10, 3, 20, new Thickness(2, 2, 2, 2), viewmodel.BtnTaster, ViewModel.ViewModel.WpfObjects.S3, $"ClkMode[{(int)ViewModel.ViewModel.WpfObjects.S3}]",ButtonBase.ClickModeProperty, grid);
+        LibWpf.LibButton.ButtonViz("S4", 2, 5, 14, 3, 20, new Thickness(2, 2, 2, 2), viewmodel.BtnTaster, ViewModel.ViewModel.WpfObjects.S4, $"ClkMode[{(int)ViewModel.ViewModel.WpfObjects.S4}]",ButtonBase.ClickModeProperty, grid);
     }
 }
