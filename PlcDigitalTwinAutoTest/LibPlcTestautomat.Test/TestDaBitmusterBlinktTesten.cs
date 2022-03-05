c73a97ff5764a6badcb1a2f79d2ebd50733ec1cf
Kurt Linder <kurt.linder@lbsfe1.snv.at>
2022-03-05 18:57:54 +0100

    -

--- PlcDigitalTwinAutoTest/LibPlcTestautomat.Test/TestDaBitmusterBlinktTesten.cs
+++ PlcDigitalTwinAutoTest/LibPlcTestautomat.Test/TestDaBitmusterBlinktTesten.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System.Diagnostics;
+using System.Threading;
 using Contracts;
 using LibDatenstruktur;
 using SoftCircuits.Silk;
@@ -8,17 +9,21 @@ namespace LibPlcTestautomat.Test;
 
 public class TestDaBitmusterBlinktTesten
 {
+    private (int _tEin, int _tPeriodendauer, byte _bitMuster) _blinkerDaten;
+
+
     public bool ThreadAktiv;
     public Datenstruktur Datenstruktur = new();
-    public (int tEin, int tAus, int bitMuster) BlinkerTuple;
+
 
     [Theory]
-    [InlineData(1, 2, "T#100ms", 0.5, 2, 0.1, "T#500ms", "kein TestKommentar", {5,5,1}, TestAnzeige.Timeout)]
+    [InlineData(1, 1, "T#100ms", 0.5, 1, 0.1, "T#1000ms", "kein TestKommentar", 50, 100, 1, TestAnzeige.Timeout)]
 
+    [InlineData(1, 2, "T#100ms", 0.5, 1, 0.1, "T#500ms", "kein TestKommentar", 25, 50, 1, TestAnzeige.Timeout)]   // Bitmuster != Bitmaske
 
-    public void TestsDaBitmusterBlinktTimeout(int bitMuster, int bitMaske, string periodendauer, float tastverhaeltnis, int anzahlPerioden, float toleranz, string timeout, string kommentar, (int tEin, int tAus, int bitMuster) blinkerTuple,  TestAnzeige testAnzeige)
+    public void TestsDaBitmusterBlinktTimeout(int bitMuster, int bitMaske, string periodendauer, float tastverhaeltnis, int anzahlPerioden, float toleranz, string timeout, string kommentar, int tEin, int tPeriodendauer, byte blinkerBitMuster, TestAnzeige testAnzeige)
     {
-        BlinkerTuple = blinkerTuple;
+        _blinkerDaten = (tEin, tPeriodendauer, blinkerBitMuster);
         ThreadAktiv = true;
         System.Threading.Tasks.Task.Run(BlinkerTask);
 
@@ -32,28 +37,28 @@ public class TestDaBitmusterBlinktTesten
         testAutomat.SetCallbackDatagridUpdaten(DatenSpeichern);
         testAutomat.FuncBitmusterBlinktTesten(args);
 
-       // Assert.Equal(kommentar, _zeile.Kommentar);
+        // Assert.Equal(kommentar, _zeile.Kommentar);
         Assert.Equal(testAnzeige, _zeile.Ergebnis);
 
-        ThreadAktiv = false;
+        // ThreadAktiv = false;
     }
-
-
     private DataGridZeile _zeile = new(0, "", TestAnzeige.CompilerErfolgreich, "", "", "", "");
-    
     private void DatenSpeichern(DataGridZeile zeile) => _zeile = zeile;
-
     private void BlinkerTask()
     {
-        Datenstruktur.Da[0] = 55;
+        var zeitMessung = new Stopwatch();
+        zeitMessung.Start();
+
+        Datenstruktur.Da[1] = 0;
+
         do
         {
-            int temp = Datenstruktur.Da[0];
+            if (zeitMessung.ElapsedMilliseconds < _blinkerDaten._tEin) Datenstruktur.Da[0] = _blinkerDaten._bitMuster;
+            else Datenstruktur.Da[0] = (byte)~_blinkerDaten._bitMuster;
 
-            Datenstruktur.Da[0] = (byte)~temp;
-            Datenstruktur.Da[1] = 0;
+            if (zeitMessung.ElapsedMilliseconds > _blinkerDaten._tPeriodendauer) zeitMessung.Restart();
 
-            Thread.Sleep(100);
+            Thread.Sleep(1);
         } while (ThreadAktiv);
     }
 }
--- PlcDigitalTwinAutoTest/LibPlcTestautomat/DaBitmusterBlinktTesten.cs
+++ PlcDigitalTwinAutoTest/LibPlcTestautomat/DaBitmusterBlinktTesten.cs
@@ -14,77 +14,93 @@ public partial class TestAutomat
         AufNegFlankeWarten = 2
     }
 
+    private struct BlinkerPeriode
+    {
+        public long Pause;
+        public long Impuls;
+        public long Periodendauer;
+        public float Tastverhaeltnis;
+
+        public BlinkerPeriode()
+        {
+            Pause = -1;
+            Impuls = -1;
+            Periodendauer = -1;
+            Tastverhaeltnis = -1;
+        }
+    }
+    private bool _messungAktiv;
+    private int _anzHalbwellen;
+    private readonly Stopwatch _periodenDauerMessen = new();
+    private readonly BlinkerPeriode[] _ergebnisse = new BlinkerPeriode[100];
+
+
     public void FuncBitmusterBlinktTesten(FunctionEventArgs args)
     {
+
         var bitMuster = args.Parameters[0].ToInteger();
         var bitMaske = args.Parameters[1].ToInteger();
-        var periodenDauer = new ZeitDauer(args.Parameters[2].ToString());
-        var tastVerhaeltnis = args.Parameters[3].ToFloat();
-        var anzahlPerioden = args.Parameters[4].ToInteger();
-        var toleranz = args.Parameters[5].ToFloat();
+        //  var periodenDauer = new ZeitDauer(args.Parameters[2].ToString());
+        // var tastVerhaeltnis = args.Parameters[3].ToFloat();
+        //  var anzahlPerioden = args.Parameters[4].ToInteger();
+        //  var toleranz = args.Parameters[5].ToFloat();
         var timeout = new ZeitDauer(args.Parameters[6].ToString());
         var kommentar = args.Parameters[7].ToString();
 
-        var periodenDauerMax = periodenDauer.DauerMs * (1 + toleranz);
-        var periodenDauerMin = periodenDauer.DauerMs * (1 - toleranz);
+        // var periodenDauerMax = periodenDauer.DauerMs * (1 + toleranz);
+        //  var periodenDauerMin = periodenDauer.DauerMs * (1 - toleranz);
 
-        var tastVerhaeltnisMax = tastVerhaeltnis * (1 + toleranz);
-        var tastVerhaeltnisMin = tastVerhaeltnis * (1 - toleranz);
+        //  var tastVerhaeltnisMax = tastVerhaeltnis * (1 + toleranz);
+        //  var tastVerhaeltnisMin = tastVerhaeltnis * (1 - toleranz);
 
-        var anzeigeUpdaten = 0;
-        const int anzeigeUpdatenSollwert = 10;
+        // var anzeigeUpdaten = 0;
+        //  const int anzeigeUpdatenSollwert = 10;
 
-        var messungAktiv = false;
-        var tastverhaeltnis = 0.0;
-        var halbwellenAnzahl = 0;
-        var zeitImpuls = 0.0;
-        var zeitPause = 0.0;
+
+        //  var tastverhaeltnis = 0.0;
+
+
         var schritte = SchritteBlinken.Starten;
-        var periodenDauerMessen = new Stopwatch();
+
         var stopwatch = new Stopwatch();
+
+
+        Array.Fill(_ergebnisse, new BlinkerPeriode());
         stopwatch.Start();
+
 
         while (stopwatch.ElapsedMilliseconds < timeout.DauerMs)
         {
-            Thread.Sleep(10);
+            Thread.Sleep(1);
 
             var digitalOutput = GetDigitalOutputWord();
-            var aktuellePeriodenDauer = zeitImpuls + zeitPause;
+            // var aktuellePeriodenDauer = zeitImpuls + zeitPause;
 
-            if (zeitImpuls > 0) tastverhaeltnis = zeitImpuls / aktuellePeriodenDauer;
+            //  if (zeitImpuls > 0) tastverhaeltnis = zeitImpuls / aktuellePeriodenDauer;
 
             switch (schritte)
             {
                 case SchritteBlinken.Starten:
-                    messungAktiv = false;
-
-                    schritte = (digitalOutput & (short)bitMaske) > 0
-                        ? SchritteBlinken.AufNegFlankeWarten
-                        : SchritteBlinken.AufPosFlankeWarten;
+                    _messungAktiv = false;
+                    schritte = (digitalOutput & (short)bitMaske) > 0 ? SchritteBlinken.AufNegFlankeWarten : SchritteBlinken.AufPosFlankeWarten;
                     break;
 
                 case SchritteBlinken.AufPosFlankeWarten:
-
                     if ((digitalOutput & (short)bitMaske) == (short)bitMuster)
                     {
-                        if (messungAktiv) zeitPause = periodenDauerMessen.ElapsedMilliseconds;
-
+                        if (_messungAktiv) _ergebnisse[_anzHalbwellen / 2].Pause = _periodenDauerMessen.ElapsedMilliseconds;
                         schritte = SchritteBlinken.AufNegFlankeWarten;
-                        periodenDauerMessen.Restart();
-                        halbwellenAnzahl++;
-                        messungAktiv = true;
+                        MessungAktiv();
                     }
                     break;
 
                 case SchritteBlinken.AufNegFlankeWarten:
                     if ((digitalOutput & (short)bitMaske) == 0)
                     {
-                        if (messungAktiv) zeitImpuls = periodenDauerMessen.ElapsedMilliseconds;
+                        if (_messungAktiv) _ergebnisse[_anzHalbwellen / 2].Impuls = _periodenDauerMessen.ElapsedMilliseconds;
 
                         schritte = SchritteBlinken.AufPosFlankeWarten;
-                        periodenDauerMessen.Restart();
-                        halbwellenAnzahl++;
-                        messungAktiv = true;
+                        MessungAktiv();
                     }
                     break;
 
@@ -92,32 +108,47 @@ public partial class TestAutomat
                     throw new ArgumentOutOfRangeException(schritte.ToString());
             }
 
-            if (anzeigeUpdaten++ <= anzeigeUpdatenSollwert) continue;
-            anzeigeUpdaten = 0;
+            /*
+                     if (anzeigeUpdaten++ <= anzeigeUpdatenSollwert) continue;
+                     anzeigeUpdaten = 0;
 
-            if (messungAktiv)
-            {
-                if (aktuellePeriodenDauer > periodenDauerMax || aktuellePeriodenDauer < periodenDauerMin)
-                {
-                    DataGridUpdaten(TestAnzeige.Fehler, (uint)bitMuster, $"{kommentar}: Falsche Periodendauer: {aktuellePeriodenDauer}ms");
-                    return;
-                }
+                     if (messungAktiv)
+                     {
+                         if (aktuellePeriodenDauer > periodenDauerMax || aktuellePeriodenDauer < periodenDauerMin)
+                         {
+                             DataGridUpdaten(TestAnzeige.Fehler, (uint)bitMuster, $"{kommentar}: Falsche Periodendauer: {aktuellePeriodenDauer}ms");
+                             return;
+                         }
 
-                if (tastverhaeltnis > tastVerhaeltnisMax || tastverhaeltnis < tastVerhaeltnisMin)
-                {
-                    DataGridUpdaten(TestAnzeige.Fehler, (uint)bitMuster, $"{kommentar}: Falsches Tastverhältnis: {tastverhaeltnis:F2}ms");
-                    return;
-                }
+                         if (tastverhaeltnis > tastVerhaeltnisMax || tastverhaeltnis < tastVerhaeltnisMin)
+                         {
+                             DataGridUpdaten(TestAnzeige.Fehler, (uint)bitMuster, $"{kommentar}: Falsches Tastverhältnis: {tastverhaeltnis:F2}ms");
+                             return;
+                         }
 
 
-                if (halbwellenAnzahl > 2 * anzahlPerioden)
-                {
-                    DataGridUpdaten(TestAnzeige.Erfolgreich, (uint)bitMuster, $"{kommentar}: E:{zeitImpuls}ms A: {zeitPause}ms → {100 * tastverhaeltnis:F1}%");
-                    return;
-                }
-            }
-            DataGridUpdaten(TestAnzeige.Aktiv, (uint)bitMuster, $"{kommentar}: I:{zeitImpuls}ms A: {zeitPause}ms → {100 * tastverhaeltnis:F1}%");
+                         if (halbwellenAnzahl > 2 * anzahlPerioden)
+                         {
+                             DataGridUpdaten(TestAnzeige.Erfolgreich, (uint)bitMuster, $"{kommentar}: E:{zeitImpuls}ms A: {zeitPause}ms → {100 * tastverhaeltnis:F1}%");
+                             return;
+                         }
+                     }
+         DataGridUpdaten(TestAnzeige.Aktiv, (uint)bitMuster, $"{kommentar}: I:{zeitImpuls}ms A: {zeitPause}ms → {100 * tastverhaeltnis:F1}%");
+                     */
+
         }
         DataGridUpdaten(TestAnzeige.Timeout, (uint)bitMuster, kommentar);
+    }
+
+    private void MessungAktiv()
+    {
+        _periodenDauerMessen.Restart();
+        _messungAktiv = true;
+
+        var i = _anzHalbwellen / 2;
+        _ergebnisse[i].Periodendauer = _ergebnisse[i].Impuls + _ergebnisse[i].Pause;
+        _ergebnisse[i].Tastverhaeltnis = _ergebnisse[i].Impuls / (float)_ergebnisse[i].Periodendauer;
+
+        _anzHalbwellen++;
     }
 }
