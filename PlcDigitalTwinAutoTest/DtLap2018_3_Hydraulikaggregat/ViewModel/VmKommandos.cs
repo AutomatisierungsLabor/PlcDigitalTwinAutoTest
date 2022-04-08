f50339280de66668184ba5fe5c8c336d75abb785
Kurt Linder <kurt.linder@lbsfe1.snv.at>
2022-04-08 10:12:21 +0200

    Hydraulikaggregat Variablen erstellt

--- PlcDigitalTwinAutoTest/DtLap2018_3_Hydraulikaggregat/ViewModel/VmKommandos.cs
+++ PlcDigitalTwinAutoTest/DtLap2018_3_Hydraulikaggregat/ViewModel/VmKommandos.cs
@@ -1,7 +1,34 @@
-﻿namespace DtLap2018_3_Hydraulikaggregat.ViewModel;
+﻿using Microsoft.Toolkit.Mvvm.Input;
+
+namespace DtLap2018_3_Hydraulikaggregat.ViewModel;
 
 public partial class VmLap2018
 {
+    [ICommand]
+    private void ButtonTaster(string taster)
+    {
+        switch (taster)
+        {
+            case "S1": (_modelLap2018.S1, ClickModeS1) = ButtonClickMode(ClickModeS1); break;
+            case "S2": (_modelLap2018.S2, ClickModeS2) = ButtonClickModeInvertiert(ClickModeS2); break;
+            case "S3":
+                (_modelLap2018.S3, ClickModeS3) = ButtonClickMode(ClickModeS3);
+                _modelLap2018.Stopwatch.Restart();
+                break;
+            case "S4": (_modelLap2018.S4, ClickModeS4) = ButtonClickMode(ClickModeS4); break;
+            case "Nachfullen": _modelLap2018.Pegel = 1; break;
+        }
+    }
 
-
+    [ICommand]
+    private void ButtonSchalter(string schalter)
+    {
+        switch (schalter)
+        {
+            case "B3": _modelLap2018.B3 = !_modelLap2018.B3; break;
+            case "B4": _modelLap2018.B4 = !_modelLap2018.B4; break;
+            case "B5": _modelLap2018.B5 = !_modelLap2018.B5; break;
+            case "F1": _modelLap2018.F1 = !_modelLap2018.F1; break;
+        }
+    }
 }
--- PlcDigitalTwinAutoTest/DtLap2018_3_Hydraulikaggregat/ViewModel/VmLap2018.cs
+++ PlcDigitalTwinAutoTest/DtLap2018_3_Hydraulikaggregat/ViewModel/VmLap2018.cs
@@ -8,48 +8,7 @@ using DtLap2018_3_Hydraulikaggregat.Model;
 using LibDatenstruktur;
 
 namespace DtLap2018_3_Hydraulikaggregat.ViewModel;
-public enum WpfObjects
-{
-    // ReSharper disable once UnusedMember.Global
-    ReserveFuerBasisViewModel = 20, // enum WpfBase
 
-    B1 = 21,
-    B2 = 22,
-    B3 = 23,
-    B4 = 24,
-    B5 = 25,
-
-    F1 = 26,
-
-    K1 = 30,
-    K2 = 31,
-
-    P1 = 40,
-    P2 = 41,
-    P3 = 42,
-    P4 = 43,
-    P5 = 44,
-    P6 = 45,
-    P7 = 46,
-    P8 = 47,
-
-    Q1 = 48,
-    Q2 = 49,
-    Q3 = 50,
-    Q4 = 51,
-
-    S1 = 55,
-    S2 = 56,
-    S3 = 57,
-    S4 = 58,
-
-    Nachfullen = 60,
-    Kurzschluss = 61,
-    OelkuehlerAbgedeckt = 62,
-    ZylinderAbgedeckt = 63,
-    OelfilterAbgedeckt = 64
-
-}
 public partial class VmLap2018 : BasePlcDtAt.BaseViewModel.VmBase
 {
     private readonly ModelLap2018 _modelLap2018;
@@ -70,10 +29,6 @@ public partial class VmLap2018 : BasePlcDtAt.BaseViewModel.VmBase
         SichtbarEin[(int)WpfBase.BtnLinkHomepageAnzeigen] = Visibility.Visible;
         SichtbarEin[(int)WpfBase.BtnAlwarmVerwaltungAnzeigen] = Visibility.Visible;
 
-        Text[(int)WpfObjects.S1] = "S1";
-        Text[(int)WpfObjects.S2] = "S2";
-        Text[(int)WpfObjects.S3] = "S3";
-        Text[(int)WpfObjects.S4] = "S4";
     }
     protected override void ViewModelAufrufThread()
     {
@@ -82,106 +37,84 @@ public partial class VmLap2018 : BasePlcDtAt.BaseViewModel.VmBase
 
         //Druck = _hydraulikaggregat.Druck;
 
-        RipFarbeUmschalten(_modelLap2018.B3, (int)WpfObjects.B3, Brushes.LawnGreen, Brushes.Red);
-        RipFarbeUmschalten(_modelLap2018.B4, (int)WpfObjects.B4, Brushes.LawnGreen, Brushes.Red);
-        RipFarbeUmschalten(_modelLap2018.B5, (int)WpfObjects.B5, Brushes.LawnGreen, Brushes.Red);
+        BrushB3 = SetBrush(_modelLap2018.B3, Brushes.LawnGreen, Brushes.Red);
+        BrushB4 = SetBrush(_modelLap2018.B4, Brushes.LawnGreen, Brushes.Red);
+        BrushB5 = SetBrush(_modelLap2018.B5, Brushes.LawnGreen, Brushes.Red);
 
-        RipFarbeUmschalten(_modelLap2018.F1, (int)WpfObjects.F1, Brushes.LawnGreen, Brushes.Red);
+        BrushF1 = SetBrush(_modelLap2018.F1, Brushes.LawnGreen, Brushes.Red);
 
-        RipFarbeUmschalten(_modelLap2018.K1, (int)WpfObjects.K1, Brushes.Red, Brushes.White);
-        RipFarbeUmschalten(_modelLap2018.K2, (int)WpfObjects.K2, Brushes.Red, Brushes.White);
+        BrushK1 = SetBrush(_modelLap2018.K1, Brushes.Red, Brushes.White);
+        BrushK2 = SetBrush(_modelLap2018.K2, Brushes.Red, Brushes.White);
 
-        RipFarbeUmschalten(_modelLap2018.P1, (int)WpfObjects.P1, Brushes.LawnGreen, Brushes.White);
-        RipFarbeUmschalten(_modelLap2018.P2, (int)WpfObjects.P2, Brushes.Red, Brushes.White);
-        RipFarbeUmschalten(_modelLap2018.P3, (int)WpfObjects.P3, Brushes.Red, Brushes.White);
-        RipFarbeUmschalten(_modelLap2018.P4, (int)WpfObjects.P4, Brushes.Red, Brushes.White);
-        RipFarbeUmschalten(_modelLap2018.P5, (int)WpfObjects.P5, Brushes.LawnGreen, Brushes.White);
-        RipFarbeUmschalten(_modelLap2018.P6, (int)WpfObjects.P6, Brushes.Red, Brushes.White);
-        RipFarbeUmschalten(_modelLap2018.P7, (int)WpfObjects.P7, Brushes.LawnGreen, Brushes.White);
-        RipFarbeUmschalten(_modelLap2018.P8, (int)WpfObjects.P8, Brushes.Red, Brushes.White);
+        BrushP1 = SetBrush(_modelLap2018.P1, Brushes.LawnGreen, Brushes.White);
+        BrushP2 = SetBrush(_modelLap2018.P2, Brushes.Red, Brushes.White);
+        BrushP3 = SetBrush(_modelLap2018.P3, Brushes.Red, Brushes.White);
+        BrushP4 = SetBrush(_modelLap2018.P4, Brushes.Red, Brushes.White);
+        BrushP5 = SetBrush(_modelLap2018.P5, Brushes.LawnGreen, Brushes.White);
+        BrushP6 = SetBrush(_modelLap2018.P6, Brushes.Red, Brushes.White);
+        BrushP7 = SetBrush(_modelLap2018.P7, Brushes.LawnGreen, Brushes.White);
+        BrushP8 = SetBrush(_modelLap2018.P8, Brushes.Red, Brushes.White);
 
-        RipFarbeUmschalten(_modelLap2018.Q1, (int)WpfObjects.Q1, Brushes.LawnGreen, Brushes.White);
-        RipFarbeUmschalten(_modelLap2018.Q2, (int)WpfObjects.Q2, Brushes.LawnGreen, Brushes.White);
-        RipFarbeUmschalten(_modelLap2018.Q3, (int)WpfObjects.Q3, Brushes.LawnGreen, Brushes.White);
-        RipFarbeUmschalten(_modelLap2018.Q4, (int)WpfObjects.Q4, Brushes.LawnGreen, Brushes.White);
+        BrushQ1 = SetBrush(_modelLap2018.Q1, Brushes.LawnGreen, Brushes.White);
+        BrushQ2 = SetBrush(_modelLap2018.Q2, Brushes.LawnGreen, Brushes.White);
+        BrushQ3 = SetBrush(_modelLap2018.Q3, Brushes.LawnGreen, Brushes.White);
+        BrushQ4 = SetBrush(_modelLap2018.Q4, Brushes.LawnGreen, Brushes.White);
 
-        RipFarbeUmschalten(_modelLap2018.S4, (int)WpfObjects.S4, Brushes.LawnGreen, Brushes.White);
+        BrushS4 = SetBrush(_modelLap2018.S4, Brushes.LawnGreen, Brushes.White);
 
-        RipSichtbarkeitUmschalten(_modelLap2018.B1, (int)WpfObjects.B1);
-        RipSichtbarkeitUmschalten(_modelLap2018.B2, (int)WpfObjects.B2);
-        RipSichtbarkeitUmschalten(_modelLap2018.B3, (int)WpfObjects.B3);
+        (VisibilityEinB1, VisibilityAusB1) = SetVisibility(_modelLap2018.B1);
+        (VisibilityEinB2, VisibilityAusB2) = SetVisibility(_modelLap2018.B2);
+        (VisibilityEinB3, VisibilityAusB3) = SetVisibility(_modelLap2018.B3);
 
-        RipSichtbarkeitUmschalten(_modelLap2018.Q2 && _modelLap2018.Q3, 40);
+        (VisibilityEinKurzschluss, _) = SetVisibility(_modelLap2018.Q2 && _modelLap2018.Q3);
 
         // Margin_1(_modelLap2018.Pegel);
-        
+
         ErweiterungOelkuehler();
         ErweiterungZylinder();
         ErweiterungOelfilter();
 
+        CheckErweiterungOelfilter();
+        CheckErweiterungOelkuehler();
+        CheckErweiterungZylinder();
     }
     internal void ErweiterungOelkuehler()
     {
-        if (SichtbarEin[(int)WpfObjects.OelkuehlerAbgedeckt] == Visibility.Visible) return;
+        if (VisibilityOelkuehlerAbgedeckt == Visibility.Visible) return;
 
         _modelLap2018.P7 = false;
     }
     internal void ErweiterungZylinder()
     {
-        if (SichtbarEin[(int)WpfObjects.ZylinderAbgedeckt] == Visibility.Visible) return;
+        if (VisibilityZylinderAbgedeckt == Visibility.Visible) return;
 
         _modelLap2018.K1 = false;
         _modelLap2018.K2 = false;
     }
     internal void ErweiterungOelfilter()
     {
-        if (SichtbarEin[(int)WpfObjects.OelfilterAbgedeckt] == Visibility.Visible) return;
+        if (VisibilityOelfilterAbgedeckt == Visibility.Visible) return;
 
         _modelLap2018.P8 = false;
     }
 
-    /*
+
     internal void CheckErweiterungOelkuehler()
     {
-        _viewModel.ViAnz.RIPSichtbarkeitUmschalten(_mainWindow.ChkOelkuehler.IsChecked != null && (bool)_mainWindow.ChkOelkuehler.IsChecked, 41);
+        // (VisibilityOelkuehlerAbgedeckt,_)=SetVisibility(_mainWindow.ChkOelkuehler.IsChecked != null && (bool)_mainWindow.ChkOelkuehler.IsChecked);
     }
     internal void CheckErweiterungZylinder()
     {
-        _viewModel.ViAnz.RIPSichtbarkeitUmschalten(
-            _mainWindow.ChZylinder.IsChecked != null && (bool)_mainWindow.ChZylinder.IsChecked, 42);
+        //  (_visibilityZylinderAbgedeckt,_)=SetVisibility(_mainWindow.ChZylinder.IsChecked != null && (bool)_mainWindow.ChZylinder.IsChecked, 42);
     }
     internal void CheckErweiterungOelfilter()
     {
-        _viewModel.ViAnz.RIPSichtbarkeitUmschalten(
-            _mainWindow.ChkOelfilter.IsChecked != null && (bool)_mainWindow.ChkOelfilter.IsChecked, 43);
+        //(_visibilityOelfilterAbgedeckt,_)=SetVisibility(_mainWindow.ChkOelfilter.IsChecked != null && (bool)_mainWindow.ChkOelfilter.IsChecked, 43);
     }
-    */
 
-    protected override void ViewModelAufrufTaster(Enum tasterId, bool gedrueckt)
-    {
-        switch (tasterId)
-        {
-            case WpfObjects.S1: _modelLap2018.S1 = gedrueckt; break;
-            case WpfObjects.S2: _modelLap2018.S2 = !gedrueckt; break;
-            case WpfObjects.S3:
-                _modelLap2018.S3 = gedrueckt;
-                _modelLap2018.Stopwatch.Restart(); break;
-            case WpfObjects.S4: _modelLap2018.S4 = gedrueckt; break;
-            case WpfObjects.Nachfullen: _modelLap2018.Pegel = 1; break;
-            default: throw new ArgumentOutOfRangeException(nameof(tasterId));
-        }
-    }
-    protected override void ViewModelAufrufSchalter(Enum schalterId)
-    {
-        switch (schalterId)
-        {
-            case WpfObjects.B3: _modelLap2018.B3 = !_modelLap2018.B3; break;
-            case WpfObjects.B4: _modelLap2018.B4 = !_modelLap2018.B4; break;
-            case WpfObjects.B5: _modelLap2018.B5 = !_modelLap2018.B5; break;
-            case WpfObjects.F1: _modelLap2018.F1 = !_modelLap2018.F1; break;
-            default: throw new ArgumentOutOfRangeException(nameof(schalterId));
-        }
-    }
+
+    protected override void ViewModelAufrufTaster(Enum tasterId, bool gedrueckt) { }
+    protected override void ViewModelAufrufSchalter(Enum schalterId) { }
     public override void PlotterButtonClick(object sender, RoutedEventArgs e) { }
     public override void BeschreibungZeichnen(TabItem tabItem) => TabZeichnen.TabZeichnen.TabBeschreibungZeichnen(this, tabItem, "#eeeeee");
     public override void LaborPlatteZeichnen(TabItem tabItem) => TabZeichnen.TabZeichnen.TabLaborPlatteZeichnen(this, tabItem, "#eeeeee");
